559111255c786e12e03280928bbb33e1c8de9d40
Ann-Kathrin Wohlrab <ann-kathrinwohlrab@imacvonnkathrin.speedport.ip>
2023-02-25 14:43:03 +0100

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,44 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback)   printTimeCallback();
+      this.currentTime++;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    const total = this.currentTime / 60;
+    const totalMinutes = Math.floor(total);
+    return totalMinutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return (("0" + value).slice(-2));
   }
 
   stop() {
-    // ... your code goes here
+    return clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes())
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+    
+   
+
+    return `${minutes}:${seconds}`
   }
 }
